4a5f59b4be41442b4b1019138d9f7125eb37253f
Lachlan <lachlan@lachlanb.com>
2021-11-03 10:34:57 +1100

    added testng dataprovider

--- Main/src/test/java/TestNGDemo.java
+++ Main/src/test/java/TestNGDemo.java
@@ -4,17 +4,30 @@ import driver.WebHandler;
 import exceptions.CustomException;
 import exceptions.UnsupportedProtocolException;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 import org.testng.annotations.Test;
 
 import javax.naming.OperationNotSupportedException;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestNGDemo {
+
+    @DataProvider(name = "data-provider")
+    public Object[][] dpMethod(){
+        return new Object[][] {
+                {
+                    AutomationPractice.model.User.getUsername(),
+                    AutomationPractice.model.User.getPassword(),
+                        "true"
+                },
+                {
+                        "Fake@username.com",
+                        "fakePassword",
+                        "false"
+                }
+        };
+    }
 
     @BeforeTest
     public void setupTest() {
@@ -22,8 +35,13 @@ public class TestNGDemo {
         WebHandler.implicitTimeout(1000);
     }
 
-    @Test
-    public void loginToWebsite() throws OperationNotSupportedException, UnsupportedProtocolException, CustomException {
+    @Test (dataProvider = "data-provider")
+    public void loginToWebsite(String[] val) throws OperationNotSupportedException, UnsupportedProtocolException, CustomException {
+        String username = val[0];
+        String password = val[1];
+        boolean outcomeExpected = Boolean.parseBoolean(val[2]);
+        AutomationPractice.model.User.setUsername(username);
+        AutomationPractice.model.User.setPassword(password);
         AutomationPractice.Service.login();
         assertTrue(Service.isLoggedIn());
         Assert.assertTrue(Service.isLoggedIn());
